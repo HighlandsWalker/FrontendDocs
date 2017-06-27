83ac84a307ea9a61eb1af92d1198b1f0801fa3c0
gabriel.tabus <ion.gabriel.tabus@gmail.com>
2017-06-27 17:37:38 +0300

    Push 8 tasks 20 plus all Exercises + 1 to 5 and some of 6

---
+++ Day1/frontdev/scripts/app2.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Gabriel.Tabus on 6/27/2017.
+ */
+var facts = [
+"Chuck Norris threw a grenade and killed 50 people, then it exploded.",
+"Chuck Norris counted to infinity. Twice.",
+"Chuck Norris can kill two stones with one bird.",
+"Chuck Norris can hear sign language."
+];
+
+
